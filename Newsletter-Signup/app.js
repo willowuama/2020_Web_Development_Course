78a9c8b69145396cfc445decd64b4033fc8054cb
willowuama <willowuama@gmail.com>
2020-04-13 22:47:15 -0500

    Finished project. Ready for deployment

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,7 +1,8 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
 const app = express();
 const port = 3000;
 
@@ -19,10 +20,52 @@ app.post('/', function(req,res){
   const lName = req.body.lName
   const email = req.body.email
 
-  console.log(`Testing ${fName} ${lName} ${email}`)
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: fName,
+          LNAME: lName
+        }
+      }
+    ]
+  };
+
+  const listID = process.env.LISTID;
+  const apiKey = process.env.APIKEY;
+  const jsonData = JSON.stringify(data);
+  const url = `https://us19.api.mailchimp.com/3.0/lists/${listID}`
+
+  const options = {
+    method: "POST",
+    auth: `BigWill:${apiKey}`
+  }
+
+  const request = https.request(url, options, function(response){
+
+    if(response.statusCode === 200){
+      res.sendFile(__dirname + "/success.html");
+    }else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+
+})
+
+app.post('/failure', function(req,res){
+  res.redirect('/');
 })
 
 // Launch Server
-app.listen(port,function(){
+app.listen(process.env.PORT || port,function(){
   console.log(`Server started on port ${port}`);
 })
